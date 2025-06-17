ec5aa5989519c2537dd6e2e1b5aa619606a48561
Nithishka25 <151643107+Nithishka25@users.noreply.github.com>
2025-06-17 07:58:44 +0530

    Add files via upload

---
+++ 10/Routes/Users.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const router = express.Router();
+const User = require("../Schema/Users");
+router.post("/create", async (req, res) => {
+  const { name, email, password, phone } = req.body;
+  try {
+    const newUser = new User({ name, email, password, phone });
+    await newUser.save();
+    res
+      .status(201)
+      .json({ message: "User created Successfully", user: newUser });
+  } catch (error) {
+    res.status(500).json({ message: "Error creating user", error });
+  }
+});
+
+
+module.exports = router;
